e70d9921c1a2e4064e0890d2570a13b718d49724
Juan José Fernández <juanjofer1999@gmail.com>
2020-05-02 22:06:17 -0400

    funcion para hallar los valores

--- lib/NorOeste/NorOeste.dart
+++ lib/NorOeste/NorOeste.dart
@@ -5,7 +5,10 @@ class NorOeste{
   List<List<double>> disponibilidad = new List<List<double>>();
   List<List<double>> matriz = new List<List<double>>();
   NorOeste(this.matriz,this.demanda,this.disponibilidad){
-
+    
+  }
+  void hallarValores(){
+    
   }
   
 }
