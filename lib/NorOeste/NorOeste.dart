23baffb8b57bffedf08aa3fee2f91817a52eb09d
Juan José Fernández <juanjofer1999@gmail.com>
2020-05-02 20:09:23 -0400

    clase NorOeste creada

---
+++ lib/NorOeste/NorOeste.dart
@@ -0,0 +1,11 @@
+import 'package:flame/game.dart';
+
+class NorOeste{
+  List<List<double>> demanda = new List<List<double>>();
+  List<List<double>> disponibilidad = new List<List<double>>();
+  List<List<double>> matriz = new List<List<double>>();
+  NorOeste(this.matriz,this.demanda,this.disponibilidad){
+
+  }
+  
+}
